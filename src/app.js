23e55c9e5282bbc8754bff621323d2a7a7751d63
vijayvaddi11 <vijayjay6688@gmail.com>
2026-04-05 07:54:27 +0530

    seperated auth router and adding rate limit

--- src/app.js
+++ src/app.js
@@ -5,35 +5,25 @@ dotenv.config({
 
 import express from "express";
 import createClient from "./db/db.js";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import verifyJWT from "./middlewares/verifyTokenMiddleware.js";
-import { createUser, fetchUserDetailsByEmail ,createTransaction, fetchTransactionsById, updateTransactionByID, softDeleteTransactionByID, deleteTransactionByID,viewTransactions} from "./db/dbFunctions.js";
 import transactionRouter from "./routes/transactionRoutes.js";
+import authRouter from './routes/authRouter.js'
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const client = await createClient(); 
 
-/**
- * transactions/
- *   - Filtering records based on criteria such as date, category, or type
- *   - Pagination
- * transactions/edit
- * transactions/create
- * transactions/delete
- * transactions/search
- *
- * dashboard/
- *
- *
- * auth/login
- * auth/register
- * auth/refreshToken
- *
- */
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes window
+  max: 100, // limit each IP to 100 requests per window
+  message: "Too many requests, please try again later."
+});
+app.use(limiter);
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 // making dbclient accessible to all routes, instead of creating a new client everytime
 app.use((req, res, next) => {
@@ -41,7 +31,9 @@ app.use((req, res, next) => {
   next();
 });
 
+
 app.use('/transactions',transactionRouter)
+app.use('/auth',authRouter)
 
 app.get("/getUserDetails", verifyJWT, async (req, res) => {
   const userRole = req.role;
@@ -59,137 +51,6 @@ app.get("/getUserDetails", verifyJWT, async (req, res) => {
   return res.json(users.rows);
 });
 
-
-app.post("/register", async (req, res) => {
-  try {
-    const { name, email, password, role } = req.body;
-
-    //user validation
-    if (!name?.trim() || !email?.trim() || !password.trim()) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ message: "require valid user details" });
-    }
-
-    if (password.trim().length < 6) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ message: "Password must be at least 6 characters" });
-    }
-
-    //check email alredy exist or not
-    const checkEmailExist = await fetchUserDetailsByEmail(client, email);
-
-    if (checkEmailExist.rowCount > 0) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ message: "Email already Exist" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const result = await createUser(client, name, email, hashedPassword, role);
-    res.json({
-      message: "user registered successfully",
-      user: result.rows[0],
-    });
-  } catch (err) {
-    console.log("Error in registering user", err);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ msg: "Error registering user" });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const existingUser = await fetchUserDetailsByEmail(client,email);
-
-    if (existingUser.rows.length === 0) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "User not found" });
-    }
-    const user = existingUser.rows[0];
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ msg: "Invalid Password" });
-    }
-
-    const accessToken = jwt.sign(
-      {
-        userId: user.id,
-        name: user.name,
-        role: user.role,
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "1h" },
-    );
-
-    const refreshToken = jwt.sign(
-      {
-        userId: user.id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-      },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "7d" },
-    );
-
-    await client.query(
-      `INSERT INTO refresh_tokens (user_id, token) VALUES ($1, $2)`,
-      [user.id, refreshToken],
-    );
-
-    return res.json({
-      message: 'Login successful',
-      accessToken,
-      refreshToken,
-    });
-  } catch (err) {
-    console.log('Error in login', err);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Login Error' });
-  }
-});
-
-
-app.post("/refreshToken", async (req, res) => {
-  const { refreshToken } = req.body;
-  if (!refreshToken) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Refresh token missing'});
-  }
-
-  try {
-    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-
-    const tokenCheck = await client.query(
-          `SELECT * FROM refresh_tokens WHERE token = $1`,
-          [refreshToken]
-     );
-     if (tokenCheck.rowCount === 0) {
-          return res.status(StatusCodes.FORBIDDEN).json({ message: 'Invalid refresh token' });
-     }
-
-    const newAccessToken = jwt.sign(
-     {
-        userId: user.id,
-        name: user.name,
-        role: user.role,
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "1h" },
-    );
-
-    res.json({
-      accessToken: newAccessToken,
-    });
-  } catch (err) {
-    res.status(403).json({ message: 'Refresh token expired or invalid'});
-  }
-});
 
 
 app.listen(process.env.PORT, () => {
